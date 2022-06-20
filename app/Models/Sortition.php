c356800ab4873ee38dc29960df1b7a43074a2de5
Samuel Lima da Silva <samuelima.00@outlook.com>
2022-06-20 19:58:43 -0300

    feat(model): Adiciona model de sorteio
    Cria representação inicial do model no banco de dados
    Cria migração pro model

---
+++ app/Models/Sortition.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Sortition extends Model
+{
+    use HasFactory;
+
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'sortitions';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'uuid';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nickname', 'description'];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'performed' => 'boolean',
+    ];
+
+
+}
