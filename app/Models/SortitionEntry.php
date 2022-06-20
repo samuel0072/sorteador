0b0ca457738540f8e2c828113edb4a2c2f453e86
Samuel Lima da Silva <samuelima.00@outlook.com>
2022-06-20 20:13:39 -0300

    feat(model): Adiciona model de entrada de um sorteio

---
+++ app/Models/SortitionEntry.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SortitionEntry extends Model
+{
+    use HasFactory;
+
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'sortition_entries';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'uuid';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['value'];
+}
---
+++ database/migrations/2022_06_20_230507_create_sortition_entries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sortition_entries', function (Blueprint $table) {
+            $table->uuid();
+            $table->string('value', 200);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->primary('uuid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sortition_entries');
+    }
+};
