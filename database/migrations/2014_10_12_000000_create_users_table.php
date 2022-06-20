abef7018e273bb82e22c21eab552dd1d06d2e9de
Samuel Lima da Silva <samuelima.00@outlook.com>
2022-06-20 20:03:07 -0300

    feat(model): Altera tipo da chave primária da tabela User
    Tipo alterado para UUID

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,15 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->primary('uuid');
         });
     }
 
