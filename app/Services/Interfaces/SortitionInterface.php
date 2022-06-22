7a88d8cc767fa03a44e798463a1b5598c4e754d2
Samuel Lima da Silva <samuelima.00@outlook.com>
2022-06-22 19:00:22 -0300

    feat(sorteio): Adiciona interface para um serviço do sorteio
    Cria interface para operações padrões sobre um sorteio

---
+++ app/Services/Interfaces/SortitionInterface.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Services\Interfaces;
+
+use App\Models\Sortition;
+use App\Models\SortitionEntry;
+use Illuminate\Support\Collection;
+
+interface SortitionInterface
+{
+    /**
+     * Persiste um sorteio e suas entradas
+     *
+     * @param Sortition $sortition O sorteio a ser persistido
+     * @param Collection $entries As entradas para serem persistidas junto do sorteio
+     * @return Sortition o sorteio persistido
+     */
+    public function createSortition(Sortition $sortition, Collection $entries): Sortition;
+
+    /**
+     * Edita as informações básicas de um sorteio
+     *
+     * @param Sortition $sortition O sorteio a ser editado
+     * @param array $data Os dados para editar
+     * @return Sortition O sorteio editado
+     */
+    public function editSortition(Sortition $sortition, array $data): Sortition;
+
+    /**
+     * Adiciona uma entrada para um sorteio
+     *
+     * @param Sortition $sortition O sorteio
+     * @param SortitionEntry $entry A entrada para ser adicionada
+     * @return SortitionEntry A entrada adicionada
+     */
+    public function addEntryToSortition(Sortition $sortition, SortitionEntry $entry): SortitionEntry;
+
+    /**
+     * Remove uma entrada de um sorteio
+     *
+     * @param Sortition $sortition O sorteio
+     * @param SortitionEntry $entry A entrada a ser removida
+     * @return bool Se a entrada foi removida ou não
+     */
+    public function removeEntryToSortition(Sortition $sortition, SortitionEntry $entry): bool;
+
+
+    /**
+     * Executa um sorteio
+     * 
+     * @param Sortition $sortition O sorteio a ser executado
+     * @return SortitionEntry A entrada sorteada
+     */
+    public function executeSortition(Sortition $sortition): SortitionEntry;
+}
