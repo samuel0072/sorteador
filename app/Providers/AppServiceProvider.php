30d9baca81bd5532f05f1d12fa29f3966faf806e
Samuel Lima da Silva <samuelima.00@outlook.com>
2022-06-22 21:34:08 -0300

    feat(sorteio): Registra implementações das interfaces de serviços no service container
    Registrado implementações default no service container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Services\DefaultSortition;
+use App\Services\GetPreSavedTextEntries;
+use App\Services\Interfaces\DrawSortitionInterface;
+use App\Services\Interfaces\GetEntriesInterface;
+use App\Services\Interfaces\SortitionInterface;
+use App\Services\SingleEntryDrawSortition;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +19,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(SortitionInterface::class, function($app) {
+            return new DefaultSortition();
+        });
+
+        $this->app->bind(GetEntriesInterface::class, function($app) {
+            return new GetPreSavedTextEntries();
+        });
+
+        $this->app->bind(DrawSortitionInterface::class, function($app) {
+            return new SingleEntryDrawSortition();
+        });
     }
 
     /**
