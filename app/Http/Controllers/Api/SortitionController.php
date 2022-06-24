8372e79afabfbb85b39a05473e097b11b5fdfb24
Samuel Lima da Silva <samuelima.00@outlook.com>
2022-06-24 09:37:59 -0300

    feat(sorteio): Adiciona validação de entries no SortitionController
    Adicionado validação do array de entries informadas no SortitionController
    Collection de entries agora é preenchida com as entries informadas
    Alterado formato da resposta do controller

--- app/Http/Controllers/Api/SortitionController.php
+++ app/Http/Controllers/Api/SortitionController.php
@@ -5,23 +5,42 @@ namespace App\Http\Controllers\Api;
 use App\Facades\Sortition as SortitionFacade;
 use App\Http\Controllers\Controller;
 use App\Models\Sortition;
+use App\Models\SortitionEntry;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
+use JetBrains\PhpStorm\ArrayShape;
 
 class SortitionController extends Controller
 {
-    public function createSortition(Request $request): Sortition
+    /**
+     * @param Request $request
+     * @return array
+     */
+    #[ArrayShape(shape: ["sortition" => "\App\Models\Sortition", "entries" => "\Illuminate\Support\Collection"])]
+    public function createSortition(Request $request): array
     {
         $request->validate([
             "nickname" => "required|string|max:250",
             "description" => "nullable|string|max:1000",
-            "entries" => "nullable|array"
+            "entries" => "nullable|array",
+            "entries.*.value" => "required|string"
         ]);
        $sortition = new Sortition;
        $sortition->nickname = $request->input("nickname");
        $sortition->description = $request->input("description");
-       $entries = new Collection();
 
-       return SortitionFacade::createSortition($sortition, $entries);
+       $entries = new Collection();
+       $rawEntries = $request->input('entries');
+
+       foreach ($rawEntries as $entry) {
+           $sortitionEntry = new SortitionEntry;
+           $sortitionEntry->value = $entry["value"];
+           $entries->add($sortitionEntry);
+       }
+
+       return [
+            "sortition" => SortitionFacade::createSortition($sortition, $entries),
+            "entries" => $entries
+        ];
     }
 }
